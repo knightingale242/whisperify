8393a7e25a11830e7fbe30342c5a0283cf0ea169
knightingale242 <92994212+knightingale242@users.noreply.github.com>
2022-07-26 20:32:23 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,25 @@
+from http import client
+import socket #how we are going to connect different instances on the same netork
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = '127.0.1.1'
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    padded_send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+client_message = input("Enter a message to send to the server:")
+send(client_message)
